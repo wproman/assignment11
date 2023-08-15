d43aca91339cf9da3d6f91fcb536b1e12cc877e2
wproman <rmn.tda@gmail.com>
2023-08-15 20:04:07 +0600

    color

--- src/app/posts/[postId]/page.jsx
+++ src/app/posts/[postId]/page.jsx
@@ -14,7 +14,7 @@ export default async function singleBlog({params}){
 
       <div className="p-4">
       
-      <h2 className="text-2xl font-bold mb-2 text-red-500">{post.title}</h2>
+      <h2 className="text-2xl font-bold mb-2 text-red-300">{post.title}</h2>
       <p className="mb-4 text-sky-600">Posted on: {post.date}</p>
       <p className=" mb-4">{post.content}</p>
       <Link  className="text-green-500 hover:underline" href="/blog">Go to Blog</Link>
